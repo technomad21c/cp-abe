4cd4b19a0ec72f6c6a61b2ff16e1f3a7f39e919e
brandon-highmarkglobaltech <brandon@highmarkglobal.tech>
2019-10-18 23:46:06 -0400

    jenkins - test.groovy

--- jenkins/test.groovy
+++ jenkins/test.groovy
@@ -2,7 +2,7 @@ properties([pipelineTriggers([githubPush()])])
 
 node {
     stage('clone') {
-        git url: "https://github.com/technomad21c/cp-abe.git", branch: "master"
+        git url: "https://github.com/technomad21c/cp-abe.git", branch: "master", , credentialId: '	25d12c3f-0c17-4d02-9e4a-fc41e2b9e437'
 
     }
 
